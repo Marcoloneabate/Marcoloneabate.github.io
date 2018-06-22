b3de57596770afa3fd9b0b90236d49f8907d026d
Marcoloneabate <39619537+Marcoloneabate@users.noreply.github.com>
2018-06-22 12:07:24 +0200

    function event

--- js/lista.js
+++ js/lista.js
@@ -5,7 +5,7 @@ console.log("Pagina lista");
 
 var SERVICE_URL = "https://jsonplaceholder.typicode.com";
 var API_KEY = "AIzaSyBZHTLXxztyPsT7KR8tD7g1inySI5cU7qQ";
-var YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBZHTLXxztyPsT7KR8tD7g1inySI5cU7qQ&q=cut";
+var YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + API_KEY + "&q=";
 
 jQuery(document).ready(function($) {
         console.log("READY")
@@ -17,9 +17,32 @@ jQuery(document).ready(function($) {
         $("#usersTable").hide();
 
         $("#searchbtn").click(function() {
-            console.log("Click");
-            getUsers();
+            console.log("Click", event);
+            // getUsers();
+            var search = $("#searchInput").val();
+            getVideos(search);
         })
+
+
+
+        $("#searchInput").keypress(function(event) {
+                console.log("tasto premuto", event.key, event.keyCode)
+                if (event.keyCode === 13) {
+                    var search = $(this).val();
+                    getVideos(search);
+                }
+
+            })
+            .focus(function(event) {
+                $(this).addClass("inputFocus")
+                console.log("Focus")
+            })
+            .blur(function() {
+                $(this).removeClass("inputFocus");
+            })
+
+
+
 
 
 
@@ -38,17 +61,18 @@ jQuery(document).ready(function($) {
         function fillTable(arrayData) {
             var $tableBody = $("#usersTable tbody");
 
-            $tableBody.html("");
-            $.each(arrayData, function(index, user) {
-                console.log(index, user)
+            $tableBody.empty(); //ripulisco il body della tabella
+            // $tableBody.html("");un altro modo per ripulire il body della tabella
+            $.each(arrayData, function(index, video) {
+                console.log(index, video)
                     //Creo una nuova riga
                 var newRow = jQuery("<tr/>");
-                //Inserisco dei tag <td>VALORE<td/>
-                newRow.append("<td>" + user.id + "<td/>") //ID
-                newRow.append("<td>" + user.name + "<td/>") //NAME
-                newRow.append("<td>" + user.email + "<td/>") //EMAIL
-                newRow.append("<td>" + user.username + "<td/>") //USERNAME
-                    //Appendo la riga alla tabello
+
+                newRow.append("<td>" + video.id.videoId + "<td/>")
+                newRow.append("<td>" + video.snippet.title + "<td/>")
+                newRow.append("<td><img src='" + video.snippet.thumbnails.default.url + "'/><td/>")
+
+                //Appendo la riga alla tabella
                 $tableBody.append(newRow);
 
             })
@@ -58,6 +82,38 @@ jQuery(document).ready(function($) {
                 $("#usersTable").fadeIn(4000)
             })
         }
+
+
+
+
+
+
+
+
+        function getVideos(search) {
+
+            $("#loadingBar").fadeIn(1000);
+            $("#emptyContent").fadeOut(1000);
+            $.getJSON(YOUTUBE_URL + search, function(response) {
+                var videos = response.items;
+                console.log("VIDEOS", videos);
+                fillTable(videos);
+            })
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
